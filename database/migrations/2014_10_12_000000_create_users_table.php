4949fbcb4e7d88413487c37c4f800c5ecee707b6
MD: Suvo <santo35702@gmail.com>
2021-12-01 17:36:21 +0600

    Users table column added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('utype')->default('USR')->comment('ADM = Admin | USR = Users');
             $table->rememberToken();
             $table->timestamps();
         });
